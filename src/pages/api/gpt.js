7881677ba36039f3b182468f8fe70b0a6b4b99d4
aovoq <a21dc163@dhw.ac.jp>
2023-03-05 13:04:04 +0900

    first commit

---
+++ src/pages/api/gpt.js
@@ -0,0 +1,22 @@
+const { Configuration, OpenAIApi } = require('openai')
+
+const handler = async (req, res) => {
+   console.log(req.body.text)
+   const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+   })
+   const openai = new OpenAIApi(configuration)
+
+   const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: req.body.text }],
+   })
+   // console.log(completion.data.choices[0].message)
+   console.log(completion.data.choices[0].message.content)
+
+   const resText = completion.data.choices[0].message.content
+   // getGpt()
+   res.status(200).json({ content: resText })
+}
+
+export default handler
